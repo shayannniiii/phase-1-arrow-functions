bf385aece4efc0e6ae4cf83c7f645aa9e3d80670
ali taslimpoor <shayannniiii@gmail.com>
2023-04-24 15:43:26 +0300

    completed

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a, b) {
+    return a / b;
+  }
+  const square = (x) => {
+    return x * x;
+  }
+  const add = (a, b) => {
+    return a + b;
+  }
+  const adding = (a, b) => a + b;
+
+module.exports = { add };
